0fc82db2ffe478a084c3f3aa595c44d4032838e7
iwag <iwagame@gmail.com>
2014-05-01 22:30:48 +0900

    impl chipmunk

--- src/myApp.js
+++ src/myApp.js
@@ -1,18 +1,19 @@
 var MyLayer = cc.LayerColor.extend({
-    helloLabel:null,
+    helloLabel_:null,
+    sprite_:null,
+    space_:null,
 
-    init:function () {
+    init:function (space) {
         this._super();
         this.setColor(cc.color(255,255,255));
+        this.space_ = space;
 
         var size = cc.director.getWinSize();
 
-        this.helloLabel = cc.LabelTTF.create("Hello Octcat", "Impact", 24);
-        this.helloLabel.setColor(cc.color(0,0,0));
-        this.helloLabel.setPosition(size.width / 2, size.height - 40);
-        this.addChild(this.helloLabel, 5);
-
-        this.addSprite(cc.p(size.width/2, size.height/2));
+        this.helloLabel_ = cc.LabelTTF.create("Hello Octcat", "Impact", 24);
+        this.helloLabel_.setColor(cc.color(0,0,0));
+        this.helloLabel_.setPosition(size.width / 2, size.height - 40);
+        this.addChild(this.helloLabel_, 5);
     },
 
     onEnter:function() {
@@ -22,27 +23,72 @@ var MyLayer = cc.LayerColor.extend({
                 this.addSprite(touch.getLocation());
             }.bind(this)
         }, this);
-        cc.eventManager.addListener({
-            event: cc.EventListener.MOUSE,
-            onMouseUp: function(event) {
-                this.addSprite(cc.p(event.getLocationX(),event.getLocationY()));
-            }.bind(this)
-        },this);
+
+
+        this._debugNode = cc.PhysicsDebugNode.create(this.space_);
+        this._debugNode.setVisible(false);
+        this.addChild(this._debugNode);
+
+
+        var size = cc.director.getWinSize();
+        this.addSprite(cc.p(size.width/2, size.height/2));
     },
 
     addSprite: function(position) {
-        var sprite = cc.Sprite.create(s_Octcat);
-        sprite.setPosition(position.x, position.y);
-        sprite.setScale(0.5);
-        this.addChild(sprite);
+        var body = new cp.Body(100, cp.momentForBox(1, 64, 64));
+        body.setPos(position);
+        this.space_.addBody(body);
+        var shape = new cp.BoxShape(body, 64, 64);
+        shape.setElasticity(0.7);
+        shape.setFriction(0.7);
+        this.space_.addShape(shape);
+
+        this.sprite_ && this.sprite_.removeFromParent() && this.sprite_.getBody().removeFromParent();
+        this.sprite_ = cc.PhysicsSprite.create(s_Octcat);
+        this.sprite_.setBody(body);
+
+        this.sprite_.setScale(0.5);
+        this.addChild(this.sprite_);
     }
 });
 
 var MyScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new MyLayer();
-        this.addChild(layer);
-        layer.init();
+
+        this.space = this.createPhysics_();
+
+        this.layer = new MyLayer();
+        this.layer.init(this.space);
+        this.addChild(this.layer);
+
+        this.scheduleUpdate();
+    },
+
+    createPhysics_: function () {
+        var space = new cp.Space();
+
+        var size = cc.director.getWinSize();
+        // Walls
+        var walls = [
+            new cp.SegmentShape(space.staticBody, cp.v(0, 0), cp.v(size.width, 0), 0),				// bottom
+            new cp.SegmentShape(space.staticBody, cp.v(0, size.height), cp.v(size.width, size.height), 0),	// top
+            new cp.SegmentShape(space.staticBody, cp.v(0, 0), cp.v(0, size.height), 0),				// left
+            new cp.SegmentShape(space.staticBody, cp.v(size.width, 0), cp.v(size.width, size.height), 0)	// right
+        ];
+        for (var i = 0; i < walls.length; i++) {
+            var shape = walls[i];
+            shape.setElasticity(1.1);
+            shape.setFriction(1.5);
+            space.addStaticShape(shape);
+        }
+
+        space.gravity = cp.v(0, -100);
+
+        return space;
+    },
+
+    update: function(dt) {
+        this.space && this.space.step(dt);
     }
 });
